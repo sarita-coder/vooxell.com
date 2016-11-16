9f6f740c1adb306301b330524484e35cff708408
sarita-coder <sarita.roman.colonio@gmail.com>
2016-11-16 09:43:19 -0500

    arreglando janrt2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-        fullpage.initialize('#fullpage', {
+      fullpage.initialize('#fullpage', {
         anchors: ['slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6'],
         menu: '#nav-parallax',
         verticalCentered: false,
@@ -14,8 +14,6 @@
     $('#menu-des').toggleClass('mover')
   })
   
-
-
 $(document).ready(function() {
 
 
@@ -40,86 +38,4 @@ $(document).ready(function() {
       $('#CE').fadeOut('xfast');
     })
 
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var logo=false;
-var menu=false;
-var scroll;
-
-    $(window).scroll(function() {
-        scroll = $(window).scrollTop();
-        if(scroll> 200){
-            if(!logo){
-            $(".VOOXELL").css({"margin-top":"2rem","width":"10rem","height":"4rem"})
-            $(".logo-text").css({"display":"none"})
-            $(".logo-vooxel").css({"padding-top":"0rem","margin":"auto 90%"})
-                logo=true;
-            }
-        }
-        else {
-            if(logo){
-            $(".VOOXELL").css({"margin-top":"30%","width":"30rem","height":"10rem"})
-            $(".logo-text").css({"display":"block"})
-            $(".logo-vooxel").css({"padding-top":"10rem","margin":"auto"})
-               logo=false;
-            }
-        }
-
-    });
-    // $(window).scroll(function() {
-    //     scroll = $(window).scrollTop();
-    //     if(scroll> 800){
-    //         if(!menu){
-    //         $(".hover-bajar").css({"color":"#C1C1C1"})
-    //         $(".hover-subir").css({"color":"#C1C1C1"})
-    //         $(".active span").css({"color":"black!important"})
-    //         $("#nav-parallax").css({"border-right":"0.01em solid rgba(216, 214, 214, 0.17)"})
-    //         $(".menu-bar").css({"border":"0.01em solid rgba(179, 170, 170, 0.91)"})
-    //         $(".bar").css({"background":"rgba(179, 170, 170, 0.91)"})
-    //             menu=true;
-    //         }
-    //     }
-
-    //     else {
-    //         if(menu){
-    //         $(".active span").css({"color":"white"})
-    //         $(".hover-bajar").css({"color":"white"})
-    //         $(".hover-subir").css({"color":"white"})
-    //         $(".hover-text").css({"color":"white"})
-    //         $("#nav-parallax").css({"border-right":"0.01em solid white"})
-    //         $(".menu-bar").css({"border":"0.01em solid white"})
-    //            menu=false;
-    //         }
-    //     }
-    // });
->>>>>>> ba0174dde9436a50f4c8d54dce958786cf90bd4f
->>>>>>> master
