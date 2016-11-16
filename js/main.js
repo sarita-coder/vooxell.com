35394f8600b9fe3472c7ffdef218918d1c693a5c
sarita-coder <sarita.roman.colonio@gmail.com>
2016-11-16 00:38:55 -0500

    arreglando fp

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+        fullpage.initialize('#fullpage', {
+        anchors: ['slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6'],
+        menu: '#nav-parallax',
+        verticalCentered: false,
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
+        //slidesNavigation: true,
+        navigation: false,
+
+
+      });
+
   $('#menu-des').on('click',function(){
     $('#main-nav').toggleClass('mostrar')
     $('#menu-des').toggleClass('mover')
@@ -30,46 +41,4 @@ $(document).ready(function() {
 });
 
 
-
-// $(window).resize(function(){
-//   var width = $(window).width();
-//   if($(window).width() >= 760){
-
-//   }
-//   else if($(window).width() < 920){
-//     $(document).on("scroll", function(){
-//     //sacamos el desplazamiento actual de la página
-//     var desplazamientoActual = $(document).scrollTop();
-//     //accedemos al control de "ir arriba"
-//     //var controlArriba = $("#irarriba");
-
-
-//        // console.log(desplazamientoActual);
-
-//     });
-//     // scroll
-//     $(".link-1").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-1").offset().top}, 2000);
-//     });
-//     $(".link-2").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-2").offset().top}, 2000);
-//     });
-//     $(".link-3").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-3").offset().top}, 2000);
-//     });
-//     $(".link-4").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-4").offset().top}, 2000);
-//     });
-//     $(".link-5").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-5").offset().top}, 2000);
-//     });
-//     $(".link-6").click(function() {
-//         $("html, body").animate({scrollTop: $(".section-6").offset().top}, 2000);
-//     });
-
-//   }
-
-// });
-
-// $(window).resize();
 
